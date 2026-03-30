12ae30085da7ce3610150640694eb186dbb4eb8e
Alexander Stach <stach.alex@yahoo.de>
2026-03-30 21:26:29 +0200

    Erweiterung des Layouts
    Kleine Anpassungen und hinzufügen von Details

---
+++ script.js
@@ -0,0 +1,5 @@
+var myName = "abc" //Im gesamten Projekt
+let ourName = "def" //nur in dem Context in dem es genutzt wird
+const pi = 3.14 //kann bzw. soll nicht verändert werden 
+
+console.log(pi);
