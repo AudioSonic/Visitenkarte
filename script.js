41494d3ca2229a5e99d0a477384afec09e4935e9
Alexander Stach <stach.alex@yahoo.de>
2026-04-02 22:49:47 +0200

    Anpassungen am JS
    Erste Implementierung der Button Funktion

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-var myName = "abc" //Im gesamten Projekt
-let ourName = "def" //nur in dem Context in dem es genutzt wird
-const pi = 3.14 //kann bzw. soll nicht verändert werden 
-
-console.log(pi);
+var ueberMichText = document.querySelector("#ueberMichText");
