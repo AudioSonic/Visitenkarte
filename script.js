558046be354948313c5efa44b084ab0653587b86
Alexander Stach <stach.alex@yahoo.de>
2026-04-03 11:20:07 +0200

    Abschluss Phase 1
    Letzte Feinjustierungen:
    - besseres HTML Layout
    - finalisierte Funktion des Scripts
    - Aufräumen des CSS

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-var ueberMichText = document.querySelector("#ueberMichText");
+const button = document.getElementById("ueberMich");
+const text = document.getElementById("ueberMichText");
+
+button.addEventListener("click", () => {
+    text.classList.toggle("active");
+});
